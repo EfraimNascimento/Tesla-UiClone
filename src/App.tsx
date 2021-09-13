17cba62d6215aba3e6ebd1a87e823b517992c15c
Efraim Nascimento <efraimteofilocamargonascimento@outlook.com>
2021-09-13 10:16:32 -0300

    estrutura de site

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { GlobalStyles } from './styles/GlobalStyles';
+import Page from './components/Page'
 
 function App() {
   return (
     <>
-      <h1>Rocketseat</h1>
+      <Page />
       
       <GlobalStyles/>
     </>
---
+++ src/components/Page/index.tsx
@@ -0,0 +1,39 @@
+import { Container } from './styles';
+import React from 'react';
+import DefaultOverlayContent from '../DefaultOverlayContent';
+import { ModelSection, ModelsWrapper } from '../Model';
+
+function Page() {
+  return (
+    <Container>
+      <ModelsWrapper>
+        <div>
+          {[
+            'Model One',
+            'Model Two',
+            'Model Three',
+            'Model Four',
+            'Model Five',
+            'Model Six',
+            'Model Seven',
+          ].map(modelName =>(
+            <ModelSection 
+            className="colored"
+            modelName = "Model One"
+            overlayNode={
+              <DefaultOverlayContent 
+                label ="Model One"
+                description="Order Online for Delivery"
+              />
+            }
+
+          />
+          ))}
+          
+        </div>
+      </ModelsWrapper>      
+    </Container>
+  );
+};
+
+export default Page;
