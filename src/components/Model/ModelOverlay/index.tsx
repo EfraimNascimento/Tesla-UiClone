603c11b65dddd4ae99232382ab89ac0d69c2baac
Efraim Nascimento <efraimteofilocamargonascimento@outlook.com>
2021-09-22 19:34:30 -0300

    getDimensions

--- src/components/Model/ModelOverlay/index.tsx
+++ src/components/Model/ModelOverlay/index.tsx
@@ -1,15 +1,41 @@
-import { ReactNode } from 'react';
-import useWrapperScroll from '../useWrapperScroll';
+import React, { useCallback, useLayoutEffect, useState } from 'react'
+import { useTransform } from 'framer-motion'
 
-import { Container } from './styles';
+import useWrapperScroll from '../useWrapperScroll'
+import { CarModel } from '../ModelsContext'
 
-interface ModelOverlayProps {
-  children: ReactNode;
+import { Container } from './styles'
+
+interface Props {
+  model: CarModel
 }
 
-function ModelOverlay({ children }: ModelOverlayProps) {
+type SectionDimensions = Pick<HTMLDivElement, 'offsetTop' | 'offsetHeight'>
 
+const ModelOverlay: React.FC<Props> = ({ model, children }) => {
   const { scrollY } = useWrapperScroll()
+
+  
+  const getSectionDimensions = useCallback(() =>{
+    return{
+      offsetTop: model.sectionRef.current?.offsetTop,
+      offsetHeight: model.sectionRef.current?.offsetHeight
+    } as SectionDimensions
+  }, [])
+  
+  const [dimensions, setDimensions] = useState<SectionDimensions>(
+    getSectionDimensions()
+  );
+
+    useLayoutEffect(() =>{
+      function onResize() {
+        window.requestAnimationFrame(() => setDimensions(getSectionDimensions()))
+      }
+
+      window.addEventListener('resize', onResize)
+
+      return () => window.removeEventListener('resize', onResize)
+    }, [])
 
   return (
 
--- src/components/Model/ModelsWrapper/index.tsx
+++ src/components/Model/ModelsWrapper/index.tsx
@@ -33,7 +33,7 @@ const ModelsWrapper: React.FC = ({ children}) => {
           
           <OverLaysRoot>
             {registeredModels.map(item =>(
-              <ModelOverlay key={item.modelName}> {item.overlayNode} </ModelOverlay>
+              <ModelOverlay key={item.modelName} model={item}>  {item.overlayNode} </ModelOverlay>
             ))}
           </OverLaysRoot>
 
