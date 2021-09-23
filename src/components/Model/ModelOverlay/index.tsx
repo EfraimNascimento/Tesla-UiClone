1003405d28335946e61fc0d8bd97d37f9e76b96d
Efraim Nascimento <efraimteofilocamargonascimento@outlook.com>
2021-09-23 20:31:04 -0300

    opacity-scroll

--- src/components/Model/ModelOverlay/index.tsx
+++ src/components/Model/ModelOverlay/index.tsx
@@ -31,15 +31,29 @@ const ModelOverlay: React.FC<Props> = ({ model, children }) => {
       function onResize() {
         window.requestAnimationFrame(() => setDimensions(getSectionDimensions()))
       }
-
+      
       window.addEventListener('resize', onResize)
 
       return () => window.removeEventListener('resize', onResize)
     }, [])
 
+    const sectionScrollProgress = useTransform(scrollY, y => (y - dimensions.offsetTop) / dimensions.offsetHeight)
+
+    const opacity = useTransform(
+      sectionScrollProgress,
+      [-0.42, -0.05, 0.05, 0.42],
+      [0, 1, 1, 0]
+
+      
+      )
+
+      const pointerEvents = useTransform(opacity, value => 
+        value > 0 ? 'auto' : 'none'  
+      )
+
   return (
 
-    <Container>
+    <Container style={{ opacity, pointerEvents }}>
       {children}
     </Container>
   );
